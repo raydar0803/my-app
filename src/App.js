4b868799e1db331f6423d0b11f73f005ac384d1a
Aindrila Ray <aindrilaray0803@gmail.com>
2023-06-15 00:25:23 +0530

    skeleton-created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname = 'container'>
+      <form classname = 'form-group'>
+        <input type="file" className='form-group' required />
+        <br></br>
+        <button type = 'submit' className='btn-btn-submit btn-lg'>Upload</button>
+      </form>
+      <br />
+      <br></br>
+      <h4>View PDF</h4>
+      <div className="pdf-container"></div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
